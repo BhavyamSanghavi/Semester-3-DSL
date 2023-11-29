799c352f5174adc626c3bea2c71bd48ec11e9d9b
Bhavyam Sanghavi <115462395+BhavyamSanghavi@users.noreply.github.com>
2023-11-29 18:58:17 +0530

    Infix to Postfix

--- Assignment 10.cpp
+++ Assignment 10.cpp
@@ -1,51 +1,61 @@
 #include <iostream>
 #include <stack>
 #include <string>
+#include <unordered_map>
+
 using namespace std;
 
 int precedence(char op) {
     if (op == '+' || op == '-')
         return 1;
-    if (op == '*' || op == '/')
+    else if (op == '*' || op == '/')
         return 2;
-    if (op == '^')
-        return 3;
     return 0;
 }
 
-string infixToPostfix(string exp) {
-    string result = "";
-    stack<char> stack;
-    for (int i = 0; i < exp.length(); i++) {
-        char c = exp[i];
-        if (isalnum(c))
-            result += c;
-        else if (c == '(')
-            stack.push('(');
-        else if (c == ')') {
-            while (!stack.empty() && stack.top() != '(') {
-                result += stack.top();
-                stack.pop();
+string infixToPostfix(const string& infix) {
+    stack<char> operators;
+    string postfix;
+    unordered_map<char, int> prec;
+
+    prec['+'] = prec['-'] = 1;
+    prec['*'] = prec['/'] = 2;
+
+    for (char c : infix) {
+        if (c == ' ' || c == '\t') continue;
+
+        if (isdigit(c) || isalpha(c)) {
+            postfix += c;
+        } else if (c == '(') {
+            operators.push(c);
+        } else if (c == ')') {
+            while (!operators.empty() && operators.top() != '(') {
+                postfix += operators.top();
+                operators.pop();
             }
-            stack.pop();
-        } else {
-            while (!stack.empty() && precedence(c) <= precedence(stack.top())) {
-                result += stack.top();
-                stack.pop();
+            operators.pop(); // Discard the '('
+        } else { // It's an operator
+            while (!operators.empty() && precedence(c) <= precedence(operators.top())) {
+                postfix += operators.top();
+                operators.pop();
             }
-            stack.push(c);
+            operators.push(c);
         }
     }
-    while (!stack.empty()) {
-        result += stack.top();
-        stack.pop();
+
+    while (!operators.empty()) {
+        postfix += operators.top();
+        operators.pop();
     }
-    return result;
+
+    return postfix;
 }
 
 int main() {
-    string exp = "(A + B) * C - (D - E) * (F + G)";
-    cout << "Infix expression: " << exp << endl;
-    cout << "Postfix expression: " << infixToPostfix(exp) << endl;
+    string infix_expression = "a + b * (c - d) / e";
+    string postfix_expression = infixToPostfix(infix_expression);
+    cout << "Infix: " << infix_expression << endl;
+    cout << "Postfix: " << postfix_expression << endl;
+
     return 0;
 }
